b04053d44b875f20beaa7ae9309654d9e909a193
paradisefilter <155768390+paradisefilter@users.noreply.github.com>
2024-01-05 22:37:38 +0330

    Update text.php

--- examples/text.php
+++ examples/text.php
@@ -6,7 +6,7 @@ use Gap\SDP\Api;
 
 $token = {HERE YOUR TOKEN};
 
-$gm = new Api($token);
+$gm = new Api($ba6f5d49b4bcad167dd4186fa86e3fe46b9c422520cccc355806e3927fcd916a);
 
 $chat_id = {HERE CHAT ID};
 
